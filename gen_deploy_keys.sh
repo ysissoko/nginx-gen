30c6b5e0836f883e87520e2c60b9c9b14117b375
Youssouf SISSOKO <youssouf.sissoko@uavia.eu>
2024-11-18 23:28:31 +0100

    add automatic ssh key generation

--- gen_deploy_keys.sh
+++ gen_deploy_keys.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+# Load Github personal access token
+source .env
+
+repo_owner=ysissoko
+
+input_json=$1
+# Get the total number of elements in the JSON array
+num_entries=$(jq 'length' $input_json)
+
 # generate a deploy key for each project and print it
-for filepath in $(cat input.example.json | jq '.[].deployment.privKeyFilepath'); do
-    ssh-keygen -t ed25519 -N -f $filepath
-    cat "$filepath.pub"
+for ((i = 0; i < num_entries; i++)); do
+    # Extract the 'privKeyFilename' and 'name' fields for the current index
+    filename=$(jq -r ".[$i].deployment.privKeyFilename" $input_json)
+    name=$(jq -r ".[$i].name" $input_json)
+
+    # if key exists continue
+    [ -f ~/.ssh/$filename ] && continue
+
+    ssh-keygen -t ed25519 -f ~/.ssh/$filename -q -P ""
+    pubkey=$(cat ~/.ssh/$filename.pub)
+
+    echo "Pubkey to deploy app $name: $pubkey"
 done
--- plopfile.js
+++ plopfile.js
@@ -1,9 +1,28 @@
 const fs = require('fs');
 const path = require('path');
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 
 
 module.exports = function (plop) {
+
+    // ...
+    plop.setActionType('exec', function (answers, config, plop) {
+        return new Promise((resolve, reject) => {
+        exec(config.command, (error, stdout, stderr) => {
+            if (error) {
+            console.error(`Errore: ${error.message}`);
+            reject(error);
+            }
+            if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            reject(new Error(stderr));
+            }
+            console.log(`Result: ${stdout}`);
+            resolve(stdout);
+        });
+        });
+    });
+
     plop.setGenerator('nginx-config', {
         description: 'Generate a single Nginx configuration file from JSON input',
         prompts: [
@@ -29,16 +48,21 @@ module.exports = function (plop) {
             return [
                 {
                     type: 'add',
-                    path: `output/nginx.conf`,
+                    path: 'output/nginx.conf',
                     templateFile: 'plop-templates/nginx-config.hbs',
                     data: { servers: jsonData }, // Pass all server data to the template
                 },
                 {
                     type: 'add',
-                    path: `output/config`,
+                    path: 'output/config',
                     templateFile: 'plop-templates/ssh-config.hbs',
                     data: { servers: jsonData }, // Pass all server data to the template
                 },
+                {
+                    type: 'exec',
+                    command: `bash gen_deploy_keys.sh ${answers.jsonPath}`,  // The script to run (replace {{scriptName}} with input value)
+                    message: 'Executing script to generate deploy keys...',
+                  }
             ];
         },
     });
